e30e2afabcd96598d443966aedd0d5eae787eae6
CyberError <hero24@icloud.com>
2015-11-09 13:15:27 +0000

    php

---
+++ hello_world.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!-- output html for the document -->
+<html>
+<head>
+<meta name="author" content="CyberError"/>
+<?php 
+/*
+ * Example of Hello World in python.
+ * @author CyberError
+ */
+// Form for inputing a name:
+function output_html($name){
+echo<<<END
+<title>Hello $name</title>
+</head>
+<body>
+<form method="GET">
+<input type="text" name="name" placeholder="Name" />
+<input type="submit" />
+</form>
+END;
+}
+// Is name being input?
+if(isset($_GET['name']) or isset($_POST['name'])){
+@$name = $_GET['name'] ?: $_POST['name'];
+output_html($name);
+echo("Hello ".$name);
+}else{
+// If not echo Hello World
+output_html("World");
+echo("Hello World");
+}
+?>
+</body>
+</html>	
