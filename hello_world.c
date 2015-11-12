0c5d3bf0b534b33f547a1cd73ad4e85461a7a221
CyberError <hero24@icloud.com>
2015-11-12 13:58:00 +0000

    Hello world in c

---
+++ hello_world.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void hello();
+int main(int argc,char** argv){
+//most basic hello world.
+printf("Hello World\n");
+//Terminal handling:
+if (argc > 1){
+//if name is provided in argv, don't ask for it
+hello(argv[1]);
+}else {
+//else ask for name
+char name[BUFSIZ];
+printf("Enter name:");
+scanf("%s",name);
+//and call hello
+hello(name);
+}
+}
+void hello(char *name){
+//function for printing hello 'name'
+printf("Hello %s\n",name);
+}
