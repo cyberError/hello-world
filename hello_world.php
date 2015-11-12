5e130ccfbc0763aebe79c39841e39e81c9d977f1
cyberError <hero24@icloud.com>
2015-11-12 14:04:06 +0000

    Hello World in php

--- hello_world.php
+++ hello_world.php
@@ -26,7 +26,7 @@ if(isset($_GET['name']) or isset($_POST['name'])){
 output_html($name);
 echo("Hello ".$name);
 }else{
-// If not echo Hello World
+// If not echo Hello World 
 output_html("World");
 echo("Hello World");
 }
